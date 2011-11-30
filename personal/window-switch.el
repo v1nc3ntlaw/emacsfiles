c4a836a7ce06fbd61110ca98855f3dbaa7d2da5e
v1nc3ntlaw <v1nc3ntlaw@gmail.com>
2011-11-30 15:19:36 +0800

    add personal settings

---
+++ personal/window-switch.el
@@ -0,0 +1,4 @@
+(global-set-key (kbd "C-x <up>") 'windmove-up)
+(global-set-key (kbd "C-x <down>") 'windmove-down)
+(global-set-key (kbd "C-x <right>") 'windmove-right)
+(global-set-key (kbd "C-x <left>") 'windmove-left)
